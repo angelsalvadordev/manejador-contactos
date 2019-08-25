46e4fb59fb3f6dbe58e6f5d8c33c71b26a65ae18
Angel Salvador <angelsalvadordev@gmail.com>
2019-08-24 22:32:25 -0500

    Updates

---
+++ precache-manifest.6e49ebc17e291a02161206cd1154405f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f950a3ad658716961fe7acfdedada4af",
+    "url": "/manejador-contenidos/index.html"
+  },
+  {
+    "revision": "f900a5346655d5b3bd71",
+    "url": "/manejador-contenidos/static/css/main.5a30c905.chunk.css"
+  },
+  {
+    "revision": "ec9e368cc2dc53e5c8eb",
+    "url": "/manejador-contenidos/static/js/2.db3fd1cb.chunk.js"
+  },
+  {
+    "revision": "f900a5346655d5b3bd71",
+    "url": "/manejador-contenidos/static/js/main.a6a68b87.chunk.js"
+  },
+  {
+    "revision": "6a2d40171685c87de6c8",
+    "url": "/manejador-contenidos/static/js/runtime~main.2083fcfc.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/manejador-contenidos/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/manejador-contenidos/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/manejador-contenidos/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  }
+]);
