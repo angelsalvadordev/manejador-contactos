5920445f6289de42b0acf649797c2927c8033e40
Angel <angelsalvadordev@gmail.com>
2020-05-21 23:00:58 -0500

    Agregado docs

---
+++ docs/precache-manifest.480ffe3ac2653be4238268b175f53a67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30161a81ff340d4beb9727e4abf7efe1",
+    "url": "/app-manejador-contactos/index.html"
+  },
+  {
+    "revision": "4f76826dda608d7aa0b1",
+    "url": "/app-manejador-contactos/static/css/main.74befe06.chunk.css"
+  },
+  {
+    "revision": "74c243fd5cbefbe1abef",
+    "url": "/app-manejador-contactos/static/js/2.1695e26c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-manejador-contactos/static/js/2.1695e26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f76826dda608d7aa0b1",
+    "url": "/app-manejador-contactos/static/js/main.2af755ae.chunk.js"
+  },
+  {
+    "revision": "57905ad299c11c8fa257",
+    "url": "/app-manejador-contactos/static/js/runtime-main.c97c95d9.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/app-manejador-contactos/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/app-manejador-contactos/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/app-manejador-contactos/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  }
+]);
